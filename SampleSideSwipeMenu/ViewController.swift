9c984ced54f77f9bd215bc80a6310a1b37f1a8e8
fjwrer_1004 <fjwrer-1004@bblab.jp>
2020-09-28 17:40:15 +0900

    Initial Commit

---
+++ SampleSideSwipeMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSideSwipeMenu
+//
+//  Created by fjwrer_1004 on 2020/09/28.
+//  Copyright © 2020 fjwrer_1004. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
